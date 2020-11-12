facd14c5eabf5470f7e5181ef99b367bf0e9a433
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-12 11:37:53 -0700

    commit tree daa67562a9c00bd0287e1e4845cb3ee0901e3d65

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    if name_hash == {}
+    return nil
+    else 
+        name_min = name_hash.first[0]
+        num_min = name_hash.first[1]
+        name_hash.collect do |name, num|
+            if num < num_min 
+                num_min = num 
+                name_min = name 
+            end
+        end
+        name_min
+    end 
 end
